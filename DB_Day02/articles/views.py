7ed56db3eb2fd4e60acb21b6dd3802b8a614c93b
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-10-06 02:03:20 +0900

    DB study

---
+++ DB_Day02/articles/views.py
@@ -0,0 +1,104 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+
+from .forms import ArticleForm, CommentForm
+from .models import Article, Comment
+
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@require_safe
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm()
+    # article 테이블에서 comment 역참조
+    comments = article.comment_set.all()
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+@require_POST
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.user.is_authenticated:
+        if request.user == article.user:
+            article.delete()
+            return redirect('articles:index')
+    return redirect('articles:detail', article.pk)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.user == article.user:
+        if request.method == 'POST':
+            form = ArticleForm(request.POST, instance=article)
+            if form.is_valid():
+                form.save()
+                return redirect('articles:detail', article.pk)
+        else:
+            form = ArticleForm(instance=article)
+    else:
+        return redirect('articles:index')
+    context = {
+        'form': form,
+        'article': article,
+    }
+    return render(request, 'articles/update.html', context)
+
+
+@require_POST
+def comments_create(request, pk):
+    if request.user.is_authenticated:
+        article = Article.objects.get(pk=pk)
+        comment_form = CommentForm(request.POST)
+        # valid 통과 이후
+        if comment_form.is_valid():
+            # 당장 저장하지 않음 -> save 했을 때 나올 결과물의 인스턴스 리턴
+            comment = comment_form.save(commit=False)
+            # article 넣어주고 저장
+            comment.article = article
+            comment.user = request.user
+            comment.save()
+        return redirect('articles:detail', article.pk)
+    return redirect('accounts:login')
+
+@require_POST
+def comments_delete(request, article_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment = Comment.objects.get(pk=comment_pk)
+        if request.user == comment.user:
+            comment.delete()
+    return redirect('articles:detail', article_pk)
